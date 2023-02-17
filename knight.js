79b84896a3fd624fb53a7b55a95d3f47ff4a6848
Renchester Ramos <renchesterjramos@gmail.com>
2023-02-18 01:02:51 +0800

    Refactor code and add documentation

--- knight.js
+++ knight.js
@@ -1,82 +1,64 @@
+/**
+ * Node factory function
+ *
+ * @param {[x, y]} coords - Takes an array with exactly two values representing the x and y position of the node
+ * @param {*} parentVal - Default: null; Links the node to a parent node if given
+ *
+ * @returns an object representing the node's position and links
+ */
+
 const PositionNode = (coords, parentVal = null) => {
   const position = coords;
   const children = [];
   const parent = parentVal;
 
-  const addChild = (child) => {
-    children.push(child);
+  const proto = {
+    addChild(child) {
+      children.push(child);
+    },
   };
 
-  return {
-    position,
-    parent,
-    children,
-    addChild,
-  };
+  return Object.assign(Object.create(proto), { position, children, parent });
 };
+
+/**
+ * Knight factory function
+ *
+ * @param {[x, y]} startPoint - Takes an array with exactly two values representing the x and y starting position of Knight
+ * @returns a Knight object with the findShortestPath method
+ */
 
 const Knight = (startPoint) => {
   let rootNode;
   let visited;
 
-  const findShortestPath = (target) => {
-    const queue = [rootNode];
-
-    while (queue.length > 0) {
-      const currNode = queue.shift();
-
-      // Check if node is target
-      if (doesPositionMatch(currNode, target)) return getPath(currNode);
-
-      const allMoves = generateMoves(currNode.position);
-      const nextMoves = allMoves.filter((move) => !hasVisited(move));
-
-      nextMoves.forEach((move) => {
-        const node = PositionNode(move, currNode);
-        currNode.addChild(node);
-
-        visited.push(node.position);
-
-        if (doesPositionMatch(node, target)) return getPath(node);
-
-        queue.push(node);
-      });
-
-      queue.push(...currNode.children);
-    }
-  };
-
-  const getPath = (node) => {
-    const path = [node.position];
-
-    while (node.parent !== null) {
-      node = node.parent;
-      path.push(node.position);
-    }
-
-    return path.reverse();
-  };
-
+  // Helper function to check if the current node is the target destination
   const doesPositionMatch = (node, target) => {
     return node.position[0] === target[0] && node.position[1] === target[1];
   };
 
-  //  Check if knight is on board
+  // Helper function to check if the position given is within the boundaries of the board
   const onBoard = (boardPosition, boardSize = 8) =>
     boardPosition[0] >= 1 &&
     boardPosition[0] <= boardSize &&
     boardPosition[1] >= 1 &&
     boardPosition[1] <= boardSize;
 
+  // Helper function to check if the position given has been visited. Compares position
+  // an array 'visited' which is a property of the knight
   const hasVisited = (boardPosition) => {
     return visited.some(
       (move) => move[0] === boardPosition[0] && move[1] === boardPosition[1],
     );
   };
 
-  const generateMoves = (boardPosition) => {
-    const [startX, startY] = boardPosition;
+  /**
+   * Generates all the possible moves a knight can take
+   * @param {[x,y]} boardPosition - Takes an array with exactly two values representing the x and y position of the knight
+   * @returns an array of moves with each move in the format *[x, y]*
+   */
 
+  const generateMoves = (boardPosition) => {
     // Valid knight move coordinates for x and y
     const validX = [-2, -1, 1, 2, -2, -1, 1, 2];
     const validY = [-1, -2, -2, -1, 1, 2, 2, 1];
@@ -84,8 +66,8 @@ const Knight = (startPoint) => {
     const validMoves = [];
 
     for (let i = 0; i <= validX.length - 1; i++) {
-      let moveX = startX + validX[i];
-      let moveY = startY + validY[i];
+      let moveX = boardPosition[0] + validX[i];
+      let moveY = boardPosition[1] + validY[i];
 
       if (onBoard([moveX, moveY])) validMoves.push([moveX, moveY]);
     }
@@ -93,6 +75,58 @@ const Knight = (startPoint) => {
     return validMoves;
   };
 
+  /**
+   * Traces the path that the knight took to get to the destination using parental links
+   * @param {{position: [x, y], parent: {}}} node
+   * @returns an array with the traversed paths *[[x1, y1], [x2, y2], [x3, y3]]*
+   */
+  const getPath = (node) => {
+    const path = [];
+
+    while (node) {
+      path.push(node.position);
+      node = node.parent;
+    }
+
+    return path.reverse();
+  };
+
+  /**
+   * Uses breadth-first search to get the shortest path of a knight from one point to another.
+   *
+   * @param {[x, y]} target - Takes an array with exactly two values representing the x and y position of the destination
+   * @returns an array of traversed paths from the starting point(root) to the destination
+   */
+
+  const findShortestPath = (target) => {
+    const queue = [rootNode];
+
+    while (queue.length > 0) {
+      const currNode = queue.shift();
+
+      // 1Check if starting point is the target
+      if (doesPositionMatch(currNode, target)) return getPath(currNode);
+
+      // Generate all possible moves then filter out valid moves on unvisited squares
+      const allMoves = generateMoves(currNode.position);
+      const nextMoves = allMoves.filter((move) => !hasVisited(move));
+
+      // Create a link between current node and next move nodes (creates a tree)
+      nextMoves.forEach((move) => {
+        const node = PositionNode(move, currNode);
+        currNode.addChild(node);
+        visited.push(node.position);
+
+        // Check if node is the target position
+        if (doesPositionMatch(node, target)) return getPath(node);
+      });
+
+      // Enqueue node children
+      queue.push(...currNode.children);
+    }
+  };
+
+  // Init function to take the place of a constructor
   const init = (startPoint) => {
     if (!onBoard(startPoint)) return 'Invalid board position';
 
@@ -111,9 +145,3 @@ const knight1 = Knight([1, 1]);
 console.log(knight1.findShortestPath([2, 3]));
 console.log(knight1.findShortestPath([2, 8]));
 console.log(knight1.findShortestPath([1, 1]));
-
-// const jerry = Knight();
-
-// const result = jerry.findShortestPath([1, 1], [4, 4]);
-
-// console.log(result);
