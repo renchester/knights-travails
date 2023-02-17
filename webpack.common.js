9d87f4e09659df3664279d4b9ae9f96bf85622de
Renchester Ramos <renchesterjramos@gmail.com>
2023-02-18 01:20:40 +0800

    Add config files

---
+++ webpack.common.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: 'img/[name][hash][ext][query]',
+    clean: true,
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+  module: {
+    rules: [
+      {
+        test: /.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /.html$/i,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['@babelpreset-env', { targets: 'defaults' }]],
+          },
+        },
+      },
+    ],
+  },
+};
