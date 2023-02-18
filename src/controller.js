c0c611247755560606ffd43a1a9383d1e886df3b
Renchester Ramos <renchesterjramos@gmail.com>
2023-02-18 23:05:27 +0800

    Add controller to return shortest path taken by knight

--- src/controller.js
+++ src/controller.js
@@ -1,3 +1,31 @@
-import style from './style.scss';
+import main from './sass/main.scss';
 
 import Knight from './model';
+import View from './view';
+
+let vertices = [];
+let knight;
+
+const controlChessSquares = (coordinates) => {
+  if (vertices.length > 0) {
+    vertices.push(coordinates);
+
+    const pathTaken = knight.findShortestPath(vertices[1]);
+    console.log(pathTaken);
+
+    View.renderPath(pathTaken);
+
+    vertices = [];
+  }
+
+  if (vertices.length < 1) {
+    knight = Knight(coordinates);
+    vertices.push(coordinates);
+  }
+};
+
+const init = () => {
+  View.addHandlerSquares(controlChessSquares);
+};
+
+init();
