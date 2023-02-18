7ac0803309318c1a4f81fcb5d810701ea9168f4b
Renchester Ramos <renchesterjramos@gmail.com>
2023-02-18 23:30:31 +0800

    Add path renderer function

--- src/view.js
+++ src/view.js
@@ -4,11 +4,19 @@ import knightIcon from './img/knight.png';
 const View = (() => {
   const squares = [...document.querySelectorAll('.board__square')];
 
-  const generateMarkup = () => ` 
+  const generateKnightIconMarkup = () => ` 
         <img
             src="${knightIcon}"
-            class="knight__logo"
-            alt="Knight Logo"/>`;
+            class="knight__icon"
+            alt="Knight Icon"/>`;
+
+  const generatePathMarkup = (i) => ` 
+            <div class="path-number__icon">${+i + 1}</div>
+              <img
+                src="${knightIcon}"
+                class="knight__icon path-visited"
+                alt="Knight Icon"
+              />`;
 
   const addHandlerSquares = (handler) => {
     squares.forEach((square) =>
@@ -17,7 +25,10 @@ const View = (() => {
         const { x, y } = chosenSquare.dataset;
 
         chosenSquare.innerHTML = '';
-        chosenSquare.insertAdjacentHTML('afterbegin', generateMarkup());
+        chosenSquare.insertAdjacentHTML(
+          'afterbegin',
+          generateKnightIconMarkup(),
+        );
 
         handler([+x, +y]);
       }),
@@ -28,7 +39,19 @@ const View = (() => {
     // Reset all squares except for an exemot square if given
   };
 
-  const renderPath = (path) => {};
+  const renderPath = (allPaths) => {
+    allPaths.forEach((path, index) => {
+      const [targetX, targetY] = path;
+
+      squares.forEach((square) => {
+        const { x, y } = square.dataset;
+
+        if (+targetX === +x && +targetY === +y) {
+          square.insertAdjacentHTML('afterbegin', generatePathMarkup(index));
+        }
+      });
+    });
+  };
 
   return {
     renderPath,
